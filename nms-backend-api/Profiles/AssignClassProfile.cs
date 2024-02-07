762c814b1409aa31bbf1183f2fb233bb2ec481ba
abiya-varghese <abiyaelsavarghese23@gmail.com>
2024-02-07 17:49:58 +0530

    commit7-9

--- nms-backend-api/Profiles/AssignClassProfile.cs
+++ nms-backend-api/Profiles/AssignClassProfile.cs
@@ -1,6 +1,14 @@
-﻿namespace nms_backend_api.Profiles
+﻿using AutoMapper;
+using nms_backend_api.DTO;
+using nms_backend_api.Entity;
+
+namespace nms_backend_api.Profiles
 {
-    public class AssignClassProfile
+    public class AssignClassProfile:Profile
     {
+        public AssignClassProfile() {
+            CreateMap<AssignClass,AssignClassDTO>();
+            CreateMap<AssignClassDTO, AssignClass>();
+        }
     }
 }
